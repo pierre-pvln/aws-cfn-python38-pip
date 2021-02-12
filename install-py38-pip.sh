75e04ead61a861bb158890911da37561b4b8725a
Pierre Veelen <pierre@pvln.nl>
2021-02-12 15:35:17 +0100

    yet another update

--- install-py38-pip.sh
+++ install-py38-pip.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+sudo apt-get install python3-pip
 python3 -m pip install --user --upgrade pip
 python3 -m venv env
 source env/bin/activate
-python3 -m pip install -r ~\requirements.txt
-python3 -m pip freeze >~\as_build.txt
+python3 -m pip install -r ~/requirements.txt
+python3 -m pip freeze >~/as_build.txt
 deactivate
