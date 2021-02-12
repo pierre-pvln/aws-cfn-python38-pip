aa81e7f66461028d23ddb178c50b24766f2fa21d
Pierre Veelen <pierre@pvln.nl>
2021-02-12 15:18:13 +0100

    venv added

--- install-py38-pip.sh
+++ install-py38-pip.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-#apt install python3-pip -y
-#pip3 install virtualenv
-#virtualenv py38-pip
-#source py38-pip/bin/activate
-#pip3 install -r requirements.txt
+python3 -m pip install --user --upgrade pip
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r ~\requirements.txt
+python3 -m pip freeze >~\as_build.txt
+deactivate
