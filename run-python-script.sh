a963f9aa849ae81043fe63902ca7041d05eaac4c
Pierre Veelen <pierre@pvln.nl>
2021-02-18 08:16:44 +0100

    folder structure changed

--- run-python-script.sh
+++ run-python-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-touch start_at_$( date '+%Y-%m-%d_%H-%M-%S' )
+touch $( date '+%Y-%m-%d_%H-%M-%S' )_app_started
 source env/bin/activate
-python3 lambda_function_test.py
+python3 ./app/lambda_function_test.py
 deactivate
-touch end_at_$( date '+%Y-%m-%d_%H-%M-%S' )
+touch $( date '+%Y-%m-%d_%H-%M-%S' )_app_ended
