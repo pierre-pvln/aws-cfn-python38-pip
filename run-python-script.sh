f930b094c70511d10eb2d968cf7dceff3ba32832
Pierre Veelen <pierre@pvln.nl>
2021-02-25 19:55:09 +0100

    yet an other update

--- run-python-script.sh
+++ run-python-script.sh
@@ -2,5 +2,9 @@
 touch $( date '+%Y-%m-%d_%H-%M-%S' )_app_started
 source env/bin/activate
 python3 ./app/lambda_function_test.py
+python3 ./app/_2_extend_baseline.py
+python3 ./app/_3_satistics_output.py
+python3 ./app/_4_bsgw_format.py
+python3 ./app/_5_sftp_to_server.py
 deactivate
 touch $( date '+%Y-%m-%d_%H-%M-%S' )_app_ended
